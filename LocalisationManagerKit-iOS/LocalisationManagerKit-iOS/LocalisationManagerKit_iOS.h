671df1b9e3c06120044edbaf4562c5cb26c52f83
Tap Payments <o.rabie@tap.company>
2020-04-19 09:12:07 +0200

    1- Created framework project. 2- Empty example project

---
+++ LocalisationManagerKit-iOS/LocalisationManagerKit-iOS/LocalisationManagerKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  LocalisationManagerKit_iOS.h
+//  LocalisationManagerKit-iOS
+//
+//  Created by Osama Rabie on 19/04/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalisationManagerKit_iOS.
+FOUNDATION_EXPORT double LocalisationManagerKit_iOSVersionNumber;
+
+//! Project version string for LocalisationManagerKit_iOS.
+FOUNDATION_EXPORT const unsigned char LocalisationManagerKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalisationManagerKit_iOS/PublicHeader.h>
+
+
