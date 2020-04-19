2d2fe796bc9b97a501fe1000d8dabf38a1659a23
Tap Payments <o.rabie@tap.company>
2020-04-19 10:03:11 +0200

    1- TapJson dynamic depth various parser

---
+++ LocalisationManagerKit-iOS/LocalisationManagerKit-iOS/Core/Public/TapLocalisationManager.swift
@@ -0,0 +1,21 @@
+//
+//  TapLocalisationManager.swift
+//  LocalisationManagerKit-iOS
+//
+//  Created by Osama Rabie on 19/04/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import Foundation
+@objc public class TapLocalisationManager: NSObject {
+    
+    @objc public static let TapLocalisationLangKey = "TapLocalisationLangKeyLocale";
+    
+    
+}
+
+
+extension NSObject {
+    
+    //let localisationKey:String?
+}
